2ce8b0d4aff9f45a41ba29c36675fdb63ad2bfe0
ReynoshSunny <reynoshsunny@gmail.com>
2018-09-05 17:42:31 +0530

    API

--- app/src/main/java/in/hashloop/mydrive/MainActivity.java
+++ app/src/main/java/in/hashloop/mydrive/MainActivity.java
@@ -1,13 +1,29 @@
 package in.hashloop.mydrive;
 
+import android.content.Intent;
+import android.sax.StartElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+Button  createbutn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button createbutn = findViewById(R.id.createbutn);
+        createbutn.setOnClickListener(v -> {
+
+            Intent i = new Intent(MainActivity.this, CreateFolderActivity.class);
+            startActivity(i);
+        });
+
+
+
     }
 }
